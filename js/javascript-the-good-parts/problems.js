d5b78945ab479855a6ca645bb3537282867dabe1
Martin Rist <mlr-github@martinrist.com>
2014-01-26 12:21:54 +0000

    Add comments before each function indicating which problem they're answering

--- js/javascript-the-good-parts/problems.js
+++ js/javascript-the-good-parts/problems.js
@@ -1,27 +1,33 @@
+// Problem 1
 function identity(x) {
     return x;
 }
 
+// Problem 2 - add
 function add(x, y) {
     return x + y;
 }
 
+// Problem 2 - mul
 function mul(x, y) {
     return x * y;
 }
 
+// Problem 3
 function identityf(x) {
     return function () {
         return x;
     }
 }
 
+// Problem 4
 function addf(x) {
     return function (y) {
         return x + y;
     }
 }
 
+// Problem 5
 function applyf(binaryFunc) {
     return function (x) {
         return function (y) {
