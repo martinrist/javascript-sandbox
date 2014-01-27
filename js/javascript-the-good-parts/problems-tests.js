6e26eaad31f53ca365050fb9346ad092e7c640ae
Martin Rist <mlr-github@martinrist.com>
2014-01-27 19:25:26 +0000

    Revert use of function form of 'use strict', as it prevents tests accessing functions declared in problems.js

--- js/javascript-the-good-parts/problems-tests.js
+++ js/javascript-the-good-parts/problems-tests.js
@@ -1,90 +1,88 @@
 /*global identity, add, mul, identityf, addf, applyf, curry, methodize, demethodize, inc_v1, inc_v2, inc_v3, twice, composeu, composeb*/
 /*global module, test, equal*/
 
-(function () {
-    'use strict';
+'use strict';
 
-    //noinspection JSUnusedGlobalSymbols
-    module("Default", {
-        teardown: function () {
-            delete Number.prototype.add;
-            delete Number.prototype.mul;
-        }
-    });
+//noinspection JSUnusedGlobalSymbols
+module("Default", {
+    teardown: function () {
+        delete Number.prototype.add;
+        delete Number.prototype.mul;
+    }
+});
 
-    test("Problem 1 - identity(x)", function () {
-        equal(identity(3), 3, "identity(x) returns argument");
-    });
+test("Problem 1 - identity(x)", function () {
+    equal(identity(3), 3, "identity(x) returns argument");
+});
 
-    test("Problem 2 - add(x, y) and mul(x, y)", function () {
-        equal(add(3, 4), 7, "add(x, y) returns x + y");
-        equal(mul(3, 4), 12, "mul(x, y) returns x * y");
-    });
+test("Problem 2 - add(x, y) and mul(x, y)", function () {
+    equal(add(3, 4), 7, "add(x, y) returns x + y");
+    equal(mul(3, 4), 12, "mul(x, y) returns x * y");
+});
 
-    test("Problem 3 - identityf(x)", function () {
-        equal(typeof identityf(3), "function", "identityf() returns a function");
-        equal(identityf(3)(), 3, "identityf() result is an identity function");
-    });
+test("Problem 3 - identityf(x)", function () {
+    equal(typeof identityf(3), "function", "identityf() returns a function");
+    equal(identityf(3)(), 3, "identityf() result is an identity function");
+});
 
-    test("Problem 4 - addf(x)(y)", function () {
-        equal(typeof addf(3), "function", "addf(x) returns a function");
-        equal(addf(3)(4), 7, "addf(x)(y) returns x + y");
-    });
+test("Problem 4 - addf(x)(y)", function () {
+    equal(typeof addf(3), "function", "addf(x) returns a function");
+    equal(addf(3)(4), 7, "addf(x)(y) returns x + y");
+});
 
-    test("Problem 5 - applyf(f)", function () {
-        equal(typeof applyf(add), "function", "applyf(f) returns a function");
-        equal(typeof applyf(add)(3), "function", "applyf(f)(x) returns a function");
-        equal(applyf(add)(3)(4), 7, "applyf(add)(x)(y) returns add(x, y)");
-        equal(applyf(mul)(5)(6), 30, "applyf(mul)(x)(y) returns mul(x, y)");
-    });
+test("Problem 5 - applyf(f)", function () {
+    equal(typeof applyf(add), "function", "applyf(f) returns a function");
+    equal(typeof applyf(add)(3), "function", "applyf(f)(x) returns a function");
+    equal(applyf(add)(3)(4), 7, "applyf(add)(x)(y) returns add(x, y)");
+    equal(applyf(mul)(5)(6), 30, "applyf(mul)(x)(y) returns mul(x, y)");
+});
 
-    test("Problem 6 - curry(f, x)", function () {
-        equal(typeof curry(add, 3), "function", "curry(f, x) returns a function");
-        equal(curry(add, 3)(4), 7, "curry(add, x)(y) returns x + y");
-        equal(curry(mul, 3)(4), 12, "curry(mul, x)(y) returns x * y");
-    });
+test("Problem 6 - curry(f, x)", function () {
+    equal(typeof curry(add, 3), "function", "curry(f, x) returns a function");
+    equal(curry(add, 3)(4), 7, "curry(add, x)(y) returns x + y");
+    equal(curry(mul, 3)(4), 12, "curry(mul, x)(y) returns x * y");
+});
 
-    test("Problem 7 - inc(x)", function () {
-        equal(typeof inc_v1, "function", "inc_v1 is a function");
-        equal(inc_v1(5), 6, "inc_v1(x) returns x + 1");
-        equal(inc_v1(inc_v1(5)), 7, "inc_v1(inc_v1(x)) returns x + 2");
-        equal(typeof inc_v2, "function", "inc_v2 is a function");
-        equal(inc_v2(5), 6, "inc_v2(x) returns x + 1");
-        equal(inc_v2(inc_v2(5)), 7, "inc_v2(inc_v2(x)) returns x + 2");
-        equal(typeof inc_v3, "function", "inc_v3 is a function");
-        equal(inc_v3(5), 6, "inc_v3(x) returns x + 1");
-        equal(inc_v3(inc_v3(5)), 7, "inc_v3(inc_v3(x)) returns x + 2");
-    });
+test("Problem 7 - inc(x)", function () {
+    equal(typeof inc_v1, "function", "inc_v1 is a function");
+    equal(inc_v1(5), 6, "inc_v1(x) returns x + 1");
+    equal(inc_v1(inc_v1(5)), 7, "inc_v1(inc_v1(x)) returns x + 2");
+    equal(typeof inc_v2, "function", "inc_v2 is a function");
+    equal(inc_v2(5), 6, "inc_v2(x) returns x + 1");
+    equal(inc_v2(inc_v2(5)), 7, "inc_v2(inc_v2(x)) returns x + 2");
+    equal(typeof inc_v3, "function", "inc_v3 is a function");
+    equal(inc_v3(5), 6, "inc_v3(x) returns x + 1");
+    equal(inc_v3(inc_v3(5)), 7, "inc_v3(inc_v3(x)) returns x + 2");
+});
 
-    test("Problem 8 - methodize(f)", function () {
-        equal(typeof methodize(add), "function", "methodize(f) returns a function");
-        Number.prototype.add = methodize(add);
-        equal((3).add(4), 7, "methodize(add) can be added to Number prototype");
-    });
+test("Problem 8 - methodize(f)", function () {
+    equal(typeof methodize(add), "function", "methodize(f) returns a function");
+    Number.prototype.add = methodize(add);
+    equal((3).add(4), 7, "methodize(add) can be added to Number prototype");
+});
 
-    test("Problem 9 - demethodize(f)", function () {
-        Number.prototype.add = methodize(add);
-        Number.prototype.mul = methodize(mul);
-        equal(typeof demethodize(Number.prototype.add), "function", "demethodize(method) returns a function");
-        equal(demethodize(Number.prototype.add)(5, 6), 11, "demethodize(Number.prototype.add)(x, y) returns x + y");
-        equal(demethodize(Number.prototype.mul)(5, 6), 30, "demethodize(Number.prototype.mul)(x, y) returns x * y");
-    });
+test("Problem 9 - demethodize(f)", function () {
+    Number.prototype.add = methodize(add);
+    Number.prototype.mul = methodize(mul);
+    equal(typeof demethodize(Number.prototype.add), "function", "demethodize(method) returns a function");
+    equal(demethodize(Number.prototype.add)(5, 6), 11, "demethodize(Number.prototype.add)(x, y) returns x + y");
+    equal(demethodize(Number.prototype.mul)(5, 6), 30, "demethodize(Number.prototype.mul)(x, y) returns x * y");
+});
 
-    test("Problem 10 - twice(f)", function () {
-        equal(typeof twice(add), "function", "twice(f) returns a function");
-        equal(twice(add)(11), 22, "twice(add)(x) returns x + x");
-        equal(twice(mul)(11), 121, "twice(mul)(x) returns x * x");
-    });
+test("Problem 10 - twice(f)", function () {
+    equal(typeof twice(add), "function", "twice(f) returns a function");
+    equal(twice(add)(11), 22, "twice(add)(x) returns x + x");
+    equal(twice(mul)(11), 121, "twice(mul)(x) returns x * x");
+});
 
-    test("Problem 11 - composeu(f, g)(x)", function () {
-        var double = twice(add);
-        var square = twice(mul);
-        equal(typeof composeu(double, square), "function", "composeu(f, g) returns a function");
-        equal(composeu(double, square)(3), 36, "composeu(f, g)(x) returns g(f(x))");
-    });
+test("Problem 11 - composeu(f, g)(x)", function () {
+    var double = twice(add);
+    var square = twice(mul);
+    equal(typeof composeu(double, square), "function", "composeu(f, g) returns a function");
+    equal(composeu(double, square)(3), 36, "composeu(f, g)(x) returns g(f(x))");
+});
 
-    test("Problem 12 - composeb(f, g)(x, y, z)", function () {
-        equal(typeof composeb(add, mul), "function", "composeb(f, g) returns a function");
-        equal(composeb(add, mul)(2, 3, 5), 25, "composeb(f, g)(x, y, z) returns g(f(x, y), z)");
-    });
-}());
+test("Problem 12 - composeb(f, g)(x, y, z)", function () {
+    equal(typeof composeb(add, mul), "function", "composeb(f, g) returns a function");
+    equal(composeb(add, mul)(2, 3, 5), 25, "composeb(f, g)(x, y, z) returns g(f(x, y), z)");
+});
--- js/javascript-the-good-parts/problems.js
+++ js/javascript-the-good-parts/problems.js
@@ -1,193 +1,191 @@
-(function () {
-    "use strict";
+"use strict";
 
-    // Problem 1
-    // Write a function that takes an argument and returns that argument:
-    //   identity(3)             // returns 3
-    function identity(x) {
+// Problem 1
+// Write a function that takes an argument and returns that argument:
+//   identity(3)             // returns 3
+function identity(x) {
+    return x;
+}
+
+
+// Problem 2
+// Write two binary functions, add and mul, that take two numbers and return their sum and product:
+//   add(3, 4)               // returns 7
+//   mul(3, 4)               // returns 12
+function add(x, y) {
+    return x + y;
+}
+
+function mul(x, y) {
+    return x * y;
+}
+
+
+// Problem 3
+// Write a function that takes an argument and returns a function that returns that argument:
+//   idf = identityf(3);
+//   idf()                   // returns 3
+function identityf(x) {
+    return function () {
         return x;
-    }
+    };
+}
 
 
-    // Problem 2
-    // Write two binary functions, add and mul, that take two numbers and return their sum and product:
-    //   add(3, 4)               // returns 7
-    //   mul(3, 4)               // returns 12
-    function add(x, y) {
+// Problem 4
+// Write a function that adds from two invocations:
+//   addf(3)(4)              // returns 7
+function addf(x) {
+    return function (y) {
         return x + y;
-    }
-
-    function mul(x, y) {
-        return x * y;
-    }
+    };
+}
 
 
-    // Problem 3
-    // Write a function that takes an argument and returns a function that returns that argument:
-    //   idf = identityf(3);
-    //   idf()                   // returns 3
-    function identityf(x) {
-        return function () {
-            return x;
-        };
-    }
-
-
-    // Problem 4
-    // Write a function that adds from two invocations:
-    //   addf(3)(4)              // returns 7
-    function addf(x) {
+// Problem 5
+// Write a function that takes a binary function, and makes it callable with two invocations:
+//   addf = applyf(add);
+//   addf(3)(4)              // returns 7
+//   applyf(mul)(5)(6)       // returns 30
+function applyf(binaryFunc) {
+    return function (x) {
         return function (y) {
-            return x + y;
+            return binaryFunc(x, y);
         };
-    }
+    };
+}
 
 
-    // Problem 5
-    // Write a function that takes a binary function, and makes it callable with two invocations:
-    //   addf = applyf(add);
-    //   addf(3)(4)              // returns 7
-    //   applyf(mul)(5)(6)       // returns 30
-    function applyf(binaryFunc) {
-        return function (x) {
-            return function (y) {
-                return binaryFunc(x, y);
-            };
-        };
-    }
+// Problem 6
+// Write a function that takes a function and an argument, and returns a function that can supply a second argument:
+//   add3 = curry(add, 3);
+//   add3(4)                 // returns 7
+//   curry(mul, 5)(6)        // returns 30
+function curry(func, x) {
+    return function (y) {
+        return func(x, y);
+    };
+}
+
+// Variant that uses applyf()
+//noinspection JSUnusedGlobalSymbols
+function curry2(func, x) {
+    return applyf(func)(x);
+}
 
 
-    // Problem 6
-    // Write a function that takes a function and an argument, and returns a function that can supply a second argument:
-    //   add3 = curry(add, 3);
-    //   add3(4)                 // returns 7
-    //   curry(mul, 5)(6)        // returns 30
-    function curry(func, x) {
-        return function (y) {
-            return func(x, y);
-        };
-    }
-
-    // Variant that uses applyf()
-    //noinspection JSUnusedGlobalSymbols
-    function curry2(func, x) {
-        return applyf(func)(x);
-    }
+// Problem 7
+// Without writing any new functions, show three ways to create the inc function:
+//   inc(5)                  // returns 6
+//   inc(inc(5))             // returns 7
+var inc_v1 = addf(1);
+var inc_v2 = applyf(add)(1);
+var inc_v3 = curry(add, 1);
 
 
-    // Problem 7
-    // Without writing any new functions, show three ways to create the inc function:
-    //   inc(5)                  // returns 6
-    //   inc(inc(5))             // returns 7
-    var inc_v1 = addf(1);
-    var inc_v2 = applyf(add)(1);
-    var inc_v3 = curry(add, 1);
+// Problem 8
+// Write methodize, a function that converts a binary function to a method:
+//   Number.prototype.add = methodize(add);
+//   (3).add(4)              // returns 7
+function methodize(func) {
+    return function (y) {
+        return func(this, y);
+    };
+}
 
 
-    // Problem 8
-    // Write methodize, a function that converts a binary function to a method:
-    //   Number.prototype.add = methodize(add);
-    //   (3).add(4)              // returns 7
-    function methodize(func) {
-        return function (y) {
-            return func(this, y);
-        };
-    }
+// Problem 9
+// Write demethodize, a function that converts a method to a binary function:
+//   demethodize(Number.prototype.add)(5, 6)
+//                           // returns 11
+
+function demethodize(func) {
+    return function (x, y) {
+        return func.call(x, y);
+    };
+}
 
 
-    // Problem 9
-    // Write demethodize, a function that converts a method to a binary function:
-    //   demethodize(Number.prototype.add)(5, 6)
-    //                           // returns 11
-
-    function demethodize(func) {
-        return function (x, y) {
-            return func.call(x, y);
-        };
-    }
-
-
-    // Problem 10
-    // Write a function twice that takes a binary function and returns a unary function that passes its argument to the
-    // binary function twice:
-    //   var double = twice(add);
-    //   double(11)              // returns 22
-    //   var square = twice(mul);
-    //   square(11)              // returns 121
-    function twice(func) {
-        return function (x) {
-            return func(x, x);
-        };
-    }
+// Problem 10
+// Write a function twice that takes a binary function and returns a unary function that passes its argument to the
+// binary function twice:
+//   var double = twice(add);
+//   double(11)              // returns 22
+//   var square = twice(mul);
+//   square(11)              // returns 121
+function twice(func) {
+    return function (x) {
+        return func(x, x);
+    };
+}
 
 
-    // Problem 11
-    // Write a function composeu that takes two unary functions and returns a unary function that calls them both:
-    //   composeu(double, square)(3)
-    //                           // returns 36
-    function composeu(f, g) {
-        return function (x) {
-            return g(f(x));
-        };
-    }
+// Problem 11
+// Write a function composeu that takes two unary functions and returns a unary function that calls them both:
+//   composeu(double, square)(3)
+//                           // returns 36
+function composeu(f, g) {
+    return function (x) {
+        return g(f(x));
+    };
+}
 
 
-    // Problem 12
-    // Write a function composeb that takes two binary functions and returns a function that calls them both:
-    //   composeb(add, mul)(2, 3, 5)
-    //                           // returns 25
-    function composeb(f, g) {
-        return function (x, y, z) {
-            return g(f(x, y), z);
-        };
-    }
+// Problem 12
+// Write a function composeb that takes two binary functions and returns a function that calls them both:
+//   composeb(add, mul)(2, 3, 5)
+//                           // returns 25
+function composeb(f, g) {
+    return function (x, y, z) {
+        return g(f(x, y), z);
+    };
+}
 
 
-    // Problem 13
-    function once1(func) {
-        var alreadyCalled = false;
-        return function () {
-            if (!alreadyCalled) {
-                alreadyCalled = true;
-                return func.apply(this, arguments);
-            } else {
-                throw Error("Function already called");
-            }
+// Problem 13
+function once1(func) {
+    var alreadyCalled = false;
+    return function () {
+        if (!alreadyCalled) {
+            alreadyCalled = true;
+            return func.apply(this, arguments);
+        } else {
+            throw Error("Function already called");
         }
     }
+}
 
-    // Problem 13 - Alternative approach without 'alreadyCalled'
-    function once2(func) {
-        return function () {
-            var f = func;
-            func = null;
-            return f.apply(this, arguments);
+// Problem 13 - Alternative approach without 'alreadyCalled'
+function once2(func) {
+    return function () {
+        var f = func;
+        func = null;
+        return f.apply(this, arguments);
+    }
+}
+
+// Problem 14
+function counterf(value) {
+    return {
+        inc: function () {
+            value += 1;
+            return value;
+        },
+        dec: function () {
+            value -= 1;
+            return value;
         }
-    }
+    };
+}
 
-    // Problem 14
-    function counterf(value) {
-        return {
-            inc: function () {
-                value += 1;
-                return value;
-            },
-            dec: function () {
-                value -= 1;
-                return value;
-            }
-        };
-    }
-
-    // Problem 15
-    function revocable(nice) {
-        return {
-            invoke: function () {
-                return nice.apply(this, arguments);
-            },
-            revoke: function () {
-                nice = null;
-            }
-        };
-    }
-}());
+// Problem 15
+function revocable(nice) {
+    return {
+        invoke: function () {
+            return nice.apply(this, arguments);
+        },
+        revoke: function () {
+            nice = null;
+        }
+    };
+}
