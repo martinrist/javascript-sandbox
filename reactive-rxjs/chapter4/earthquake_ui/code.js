58c6f940235d483dacd9e749176831a380d445f8
Martin Rist <mlr-github@martinrist.com>
2017-08-08 13:38:16 +0100

    Convert to ES6 arrow notation

--- reactive-rxjs/chapter4/earthquake_ui/code.js
+++ reactive-rxjs/chapter4/earthquake_ui/code.js
@@ -3,20 +3,15 @@
 function initialise() {
     const quakes = Rx.Observable
         .interval(5000)
-        .flatMap(function () {
-            return Rx.DOM.jsonpRequest({
+        .flatMap(() =>
+            Rx.DOM.jsonpRequest({
                 url: QUAKE_URL,
                 jsonpCallback: "eqfeed_callback"
-            }).retry(3);
-        })
-        .flatMap(function (result) {
-            return Rx.Observable.from(result.response.features);
-        })
-        .distinct(function (quake) {
-            return quake.properties.code;
-        });
+            }).retry(3))
+        .flatMap(result => Rx.Observable.from(result.response.features))
+        .distinct(quake => quake.properties.code);
 
-    quakes.subscribe(function (quake) {
+    quakes.subscribe(quake => {
         const coords = quake.geometry.coordinates;
         const size = quake.properties.mag * 10000;
         L.circle([coords[1], coords[0]], size).addTo(theMap);
@@ -26,9 +21,7 @@ function initialise() {
     quakes
         .pluck("properties")
         .map(makeRow)
-        .subscribe(function(row) {
-            table.appendChild(row);
-        });
+        .subscribe(row => table.appendChild(row));
 }
 
 Rx.DOM.ready().subscribe(initialise());
@@ -40,7 +33,7 @@ function makeRow(props) {
 
     const date = new Date(props.time);
     const time = date.toString();
-    [props.place, props.mag, time].forEach(function(text) {
+    [props.place, props.mag, time].forEach(text => {
         const cell = document.createElement("td");
         cell.textContent = text;
         row.appendChild(cell);
