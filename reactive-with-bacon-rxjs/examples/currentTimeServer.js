f5c0d462ef0364e863f8cb37cc18b09656a7dafe
Martin Rist <mlr-github@martinrist.com>
2017-09-04 09:33:20 +0100

    Remove directory that's been extracted to its own repo

--- reactive-with-bacon-rxjs/examples/currentTimeServer.js
+++
@@ -1,9 +0,0 @@
-/* global require */
-
-const http = require("http");
-
-http.createServer((req, res) => {
-   res.writeHead(200, {"Content-Type": "text/plain"});
-   res.write(new Date().toISOString());
-   res.end();
-}).listen(9876);
--- reactive-with-bacon-rxjs/examples/example9a-subject-rxjs.js
+++
@@ -1,9 +0,0 @@
-/* global require */
-
-const Rx = require("rx");
-
-const source = Rx.Observable.interval(1000);
-const subject = new Rx.Subject();
-source.subscribe(subject);
-
-subject.subscribe(e => console.log);
